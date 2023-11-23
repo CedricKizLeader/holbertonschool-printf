42dc121adf3fbabafdb4f8c76f3d9a3389dd33fb
Horalta <quaggiomathys@gmail.com>
2023-11-23 04:55:31 -0800

    task

--- fonction.c
+++ fonction.c
@@ -1,45 +1,52 @@
-#include <main.h>
+#include <unistd.h>  // Ajout de l'en-tête pour la fonction write
+#include <stdarg.h>
 
-/**
- * _printf - Custom printf function with limited functionality
- * @format: Format string containing directives
- *
- * Return: Number of characters printed (excluding null byte)
- */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+int print_str(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        _putchar(*str);
+        count++;
+        str++;
+    }
+    return count;
+}
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
+    if (format == NULL)
+        return -1;
 
-	va_start(args, format);
+    va_list args;
+    va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
+    int count = 0;
 
-	while (*format != '\0')
-	{
-		if (*format == '%' && *(format + 1) == 's')
-		{
-			char *str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(null)";
-			while (*str != '\0')
-			{
-				putchar(*str);
-				count++;
-				str++;
-			}
-			format += 2;
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-			format++;
-		}
-	}
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) == 's')
+        {
+            char *str = va_arg(args, char *);
+            if (str == NULL)
+                str = "(null)";
 
-	va_end(args);
-	return (count);
+            count += print_str(str);
+            format += 2;
+        }
+        else
+        {
+            _putchar(*format);
+            count++;
+            format++;
+        }
+    }
+
+    va_end(args);
+    return count;
 }
