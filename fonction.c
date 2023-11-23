9a938fd6b2d3817655a7e0bc924dabadd8bb9664
Horalta <quaggiomathys@gmail.com>
2023-11-23 00:17:09 -0800

    task

---
+++ fonction.c
@@ -0,0 +1,46 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - Custom printf function with limited functionality
+ * @format: Format string containing directives
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while (*format != '\0')
+	{
+		if (*format == '%' && *(format + 1) == 's')
+		{
+			char *str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(null)";
+			while (*str != '\0')
+			{
+				putchar(*str);
+				count++;
+				str++;
+			}
+			format += 2;
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+			format++;
+		}
+	}
+
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef PROTO
+#define PROTO
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+#endif
