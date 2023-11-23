759669c3c1f31aeed21e1d80cd253e556a1a402d
cedricswing <144893876+cedricswing@users.noreply.github.com>
2023-11-23 16:03:23 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,38 @@
 #include "main.h"
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
+	va_list args;
+	int count = 0;
+	char nextchar;
+
+	if (format == NULL)
+		return -1;
+
+	va-start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+
+			nextchar = *(format + 1);
+	                if (nextchar == 'c')
+		            count += print_char(args);
+	                if (nextchar == '%')
+		            count += print_pourcent();
+	                if (nextchar == 's')
+		            count += print_string(args);
+
+	                format += 1;
+	}
+	else
+	{
+		_putchar(*format);
+		count++;
+	}
+	format++;
+}
+
+va_end(args);
+return count;
 }
